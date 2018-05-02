675205dec2de3d7ca8a43b943bd1f8524438c367
ickythumped <roy.gaurav9421@gmail.com>
2018-05-02 17:27:44 -0500

    Chapter dictionaries created

--- nest.py
+++ nest.py
@@ -5,6 +5,7 @@ Created on Tue May  1 23:05:06 2018
 @author: Gaurav Roy
 """
 #%% Imports
+
 import re
 from collections import defaultdict
 from collections import OrderedDict
@@ -12,6 +13,7 @@ from nltk.tokenize import sent_tokenize, word_tokenize, RegexpTokenizer
 from itertools import count
 
 #%% Strings
+
 filename = "body_of_text.txt"
 pattern_book = "BOOK"
 pattern_epilogue1 = "FIRST EPILOGUE"
@@ -23,6 +25,7 @@ regex_chapter = r'\s+(.*?)\s+'
 regex_lastitem = r'\s+(.*)$'
 
 #%% List Declarations
+
 book_content = []
 chapter_content = []
 chapter_paras = []
@@ -34,6 +37,7 @@ list_books = []
 list_chapters = []
     
 #%% Reading File / list of book titles / list of chapter titles
+
 with open(filename, "r", encoding="UTF8") as file:
     data = file.read().replace('\n', ' ')
             
@@ -49,7 +53,8 @@ with open(filename, "r", encoding="UTF8") as file:
         if re.match(pattern_chapter, l):
             list_chapters.append(l.strip('\n'))
 
-#%%  Creating content lists of books, chapters        
+#%%  Creating content lists of books, chapters
+
 counter = 0
 for x in range(0,len(list_books)-1):           
     regex1 = re.compile(list_books[x] + regex_book + list_books[x+1])
@@ -65,7 +70,8 @@ for x in range(0,len(list_books)-1):
     chapter_content.append(re.findall(regex3, book_content[x][0]))    
     counter = counter + current_count
 
-#%% Creating content for last book    
+#%% Creating content for last book
+    
 regex4 = re.compile(list_books[x+1] + regex_lastitem)
 book_content.append(re.findall(regex4, data))
 current_count = book_content[x+1][0].count("CHAPTER")
@@ -80,14 +86,17 @@ chapter_content.append(re.findall(regex3, book_content[x+1][0]))
 counter = counter + current_count
   
 #%% Creating paras, sentences and words lists
+
+## Creating chapter of paras list
 for w in range(0, len(chapter_content)):
     chapter_paras.append(chapter_content[w][0].split("  "))
 
-    
+## Creating para of sentences list 
 for s in range(0, len(chapter_paras)):
     for t in range(0, len(chapter_paras[s])):
         para_sentences.append(sent_tokenize(chapter_paras[s][t]))
 
+## Creating sentence of words list
 wordonly_tokenizer = RegexpTokenizer(r'\w+')        
 for u in para_sentences:
     for v in range(0, len(u)):
@@ -96,29 +105,29 @@ for u in para_sentences:
 #%% Creating dictionaries
 
 ## Creating word dictionary
-word_dict = defaultdict(dict)  
+word_dict = {}  
 for i, sentence in enumerate(sentence_words):
     tempdict1 = {}
     for j in range(0, len(sentence)):
         tempdict1[j+1] = sentence[j]
+    
     word_dict[i+1] = tempdict1   
 
 ## Creating sentence and para dictionaries
-sentence_dict= defaultdict(list)
-para_dict = defaultdict()
-sequence = count(start = 1, step = 1)
+para_dict = {}
+sequence1 = count(start = 1, step = 1)
 for k in range(0, len(para_sentences)):
-    tempdict2 = {}
+    sentence_dict= {}
     for m in range(0, len(para_sentences[k])):
         sentence_dict.setdefault(m+1, []).append(para_sentences[k][m])
-        sentence_dict.setdefault(m+1, []).append(word_dict[next(sequence)])
+        sentence_dict.setdefault(m+1, []).append(word_dict[next(sequence1)])
     para_dict[k+1] = sentence_dict
 
 ## Creating chapter dictionaries
-
-
-
-
-
-
-        
+chapter_dict = {}
+sequence2 = count(start = 1, step = 1)
+for n in range(0, len(chapter_paras)):
+    tempdict2 = {}
+    for p in range(0, len(chapter_paras[n])):
+        tempdict2[p+1] = para_dict[next(sequence2)]
+    chapter_dict[n+1] =  tempdict2
