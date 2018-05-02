a360512e86fbc2f3b96fa58cef2efd6b02c789ff
ickythumped <roy.gaurav9421@gmail.com>
2018-05-02 13:51:12 -0500

    Created sentence list and word list

--- nest.py
+++ nest.py
@@ -4,30 +4,38 @@ Created on Tue May  1 23:05:06 2018
 
 @author: Gaurav Roy
 """
-
+#%% Imports
 import re
 from collections import defaultdict
 from collections import OrderedDict
-from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.tokenize import sent_tokenize, word_tokenize, RegexpTokenizer
 
+#%% Strings
 filename = "body_of_text.txt"
 pattern_book = "BOOK"
+pattern_epilogue1 = "FIRST EPILOGUE"
+pattern_epilogue2 = "SECOND EPILOGUE"
 pattern_chapter = "CHAPTER"
 
-regex_book = r'(.*?)'
-regex_chapter = r'(.*?)'
-regex_lastchapter = r'\s+(.*)$'
+regex_book = r'\s+(.*?)\s+'
+regex_chapter = r'\s+(.*?)\s+'
+regex_lastitem = r'\s+(.*)$'
 #regex_para = r'()
 
+#%% List Declarations
 book_content = []
 chapter_content = []
 chapter_paras = []
 para_sentences = []
+sentence_words = []
 para_content = []
 list_words = []
 list_books = []
 list_chapters = []
+
+tempdict1 = {}
           
+#%% Reading File / list of book titles / list of chapter titles
 with open(filename, "r", encoding="UTF8") as file:
     data = file.read().replace('\n', ' ')
             
@@ -36,9 +44,14 @@ with open(filename, "r", encoding="UTF8") as file:
     for l in file:
         if re.match(pattern_book, l):
             list_books.append(l.strip('\n'))
+        if re.match(pattern_epilogue1, l):
+            list_books.append(l.strip('\n'))
+        if re.match(pattern_epilogue2, l):
+            list_books.append(l.strip('\n'))    
         if re.match(pattern_chapter, l):
             list_chapters.append(l.strip('\n'))
-            
+
+#%%  Creating content lists of books, chapters        
 count = 0
 for x in range(0,len(list_books)-1):           
     regex1 = re.compile(list_books[x] + regex_book + list_books[x+1])
@@ -54,12 +67,35 @@ for x in range(0,len(list_books)-1):
         #print(regex2)
         chapter_content.append(re.findall(regex2, book_content[x][0]))
 
-    regex3 = re.compile(list_chapters[y+1] + regex_lastchapter) #need to change regular expression
-    print(regex3)
+## Last chapters
+    regex3 = re.compile(list_chapters[y+1] + regex_lastitem)
+    #print(regex3)
     chapter_content.append(re.findall(regex3, book_content[x][0]))    
     count = count + current_count
     #print(count)
+
+#%% Creating content for last book    
+regex4 = re.compile(list_books[x+1] + regex_lastitem)
+    #print(regex1)
+book_content.append(re.findall(regex4, data))
+current_count = book_content[x+1][0].count("CHAPTER")
+#print(current_count)
     
+for y in range(count, count + current_count-1):
+    #print (list_chapters[y+1])
+        
+    regex2 = re.compile(list_chapters[y] + regex_chapter + list_chapters[y+1])
+    #print(regex2)
+    chapter_content.append(re.findall(regex2, book_content[x+1][0]))
+
+## Last Chapter
+regex3 = re.compile(list_chapters[y+1] + regex_lastitem) #need to change regular expression
+#print(regex3)
+chapter_content.append(re.findall(regex3, book_content[x+1][0]))    
+count = count + current_count
+#print(count)
+    
+#%%
 for w in range(0, len(chapter_content)):
     chapter_paras.append(chapter_content[w][0].split("  "))
 
@@ -70,5 +106,15 @@ for s in range(0, len(chapter_paras)):
         #print(chapter_paras[s][t])
 #        for u in range(t.count('.')):
         para_sentences.append(sent_tokenize(chapter_paras[s][t]))
+
+wordonly_tokenizer = RegexpTokenizer(r'\w+')        
+for u in para_sentences:
+    for v in range(0, len(u)):
+        sentence_words.append(wordonly_tokenizer.tokenize(u[v]))
         
-    
+#%%    
+
+for i, sentence in enumerate(sentence_words):
+    for j in range(0, len(sentence)):
+        tempdict1[j] = sentence[i][j]
+        
