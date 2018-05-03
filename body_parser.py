1f8f2c8dcaef1e7a40f6ce7145bda4734c73e6ec
ickythumped <roy.gaurav9421@gmail.com>
2018-05-02 23:59:05 -0500

    added try block to body_parser

--- body_parser.py
+++ body_parser.py
@@ -11,11 +11,14 @@ parser_start = "BOOK ONE: 1805\n"
 parser_end = "End of the Project Gutenberg EBook of War and Peace, by Leo Tolstoy\n"
 flag = 0
 
-with open(filename, "r", encoding="UTF8") as file1, open(body, "x", encoding="UTF8") as file2:
-    for line in file1:
-        if line == parser_start:
-            flag = 1
-        if line == parser_end:
-            break
-        if flag == 1:
-            file2.write(line)
+try:
+    with open(filename, "r", encoding="UTF8") as file1, open(body, "x", encoding="UTF8") as file2:
+        for line in file1:
+            if line == parser_start:
+                flag = 1
+            if line == parser_end:
+                break
+            if flag == 1:
+                file2.write(line)   
+except IOError:
+        print ("File already exists")
