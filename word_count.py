08e9186160cb94392f9653664329f9ca2b90b127
ickythumped <roy.gaurav9421@gmail.com>
2018-05-04 01:10:14 -0500

    Used list comprehension

--- word_count.py
+++ word_count.py
@@ -14,33 +14,21 @@ from nltk.probability import FreqDist
 
 dict_books = nested_structure()
 list_sentences = listof_sentences(dict_books)
-words_sentences = []
-words_list = []
-words_filtered = []
-words_lemmatized = []
-
-
 
 wordonly_tokenizer = RegexpTokenizer(r'\w+')
-for sentence in list_sentences:
-    words_sentences.append(wordonly_tokenizer.tokenize(sentence))
+words_sentences = [wordonly_tokenizer.tokenize(sentence) for sentence in list_sentences]
 
-for element in words_sentences:
-    for word in element:
-        words_list.append(word)
+words_list = [word for element in words_sentences for word in element]
 
-stopWords = set(stopwords.words('english'))        
-for w in words_list:
-    if w not in stopWords:
-        words_filtered.append(w)
-        
+stopWords = set(stopwords.words('english'))   
+words_filtered = [w for w in words_list if w not in stopWords]     
+
 words_alpha = [wa for wa in words_filtered if not any(c.isdigit() for c in wa)]
      
 lemmatizer = WordNetLemmatizer()
-for wf in words_alpha:
-    words_lemmatized.append(lemmatizer.lemmatize(wf))
+words_lemmatized = [lemmatizer.lemmatize(wf) for wf in words_alpha]
     
-fdist = FreqDist(wl.lower() for wl in words_lemmatized)
+table_wordcount = FreqDist(wl.lower() for wl in words_lemmatized)
     
 
 
