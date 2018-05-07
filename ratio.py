824718cdaa946d3d25701a3c959971fd240c2fba
ickythumped <roy.gaurav9421@gmail.com>
2018-05-06 22:02:58 -0500

    minor

--- ratio.py
+++ ratio.py
@@ -8,26 +8,25 @@ Created on Wed May  2 20:30:42 2018
 #%% Imports
 import re
 
-#%% Function to convert dict to list
+#%% Function to convert dict to list of sentences
 def listof_sentences(dict_books):
     
-#    listof_s = [sentence[0] for chapter in dict_books.values() for para in chapter.values() for sentn \
-#                in para.values() for sentence in sentn.values()]
-#    
-#    return(listof_s)
+    listof_s = [sentence[0] for chapter in dict_books.values() for para in chapter.values() for sentn \
+                in para.values() for sentence in sentn.values()]
     
-    listof_sentn = [] 
-    listof_s = []
+    return(listof_s)
+  
+#%% Function to group sentences by books    
+def listof_chapsent(dict_books): 
     listof_chdicts = [[] for _ in range(0, len(dict_books))]
     for book_ix, chapter in enumerate(dict_books.values()):
         for para in chapter.values():
             for sentn in para.values():
                 for sentence in sentn.values():
-                    listof_sentn.append(sentence[0])
                     listof_chdicts[book_ix].append(sentence[0])
     
-    listof_s = [listof_sentn] + listof_chdicts    
-    return(listof_s)
+       
+    return(listof_chdicts)
 
         
 #%% Function for calculating Ratio of Direct speech to Indirect speech
